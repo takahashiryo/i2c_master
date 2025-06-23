cc63514cb28ffb8ea4a9081d8e80d988eac10d27
takahashiryo <35280636+takahashiryo@users.noreply.github.com>
2025-06-23 22:18:14 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+// エラーメッセージを表示する関数
+void printI2CError(uint8_t error, uint8_t address)
+{
+  Serial.print("I2C Error at address 0x");
+  if (address < 16)
+    Serial.print("0");
+  Serial.print(address, HEX);
+  Serial.print(": ");
+
+  switch (error)
+  {
+  case 0:
+    Serial.println("Success");
+    break;
+  case 1:
+    Serial.println("Data too long to fit in transmit buffer");
+    break;
+  case 2:
+    Serial.println("Received NACK on transmit of address");
+    break;
+    Serial.println("Received NACK on transmit of data");
+    break;
+  case 4:
+    Serial.println("Other error");
+    break;
+  case 5:
+    Serial.println("Timeout");
+    break;
+  default:
+    Serial.print("Unknown error code: ");
+    Serial.println(error);
+    break;
+  }
+}
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -47,16 +82,19 @@ void loop()
   // 固定アドレス0x08からデータを読み取り
   for (uint8_t address = 1; address <= 30; address++)
   {
+    delay(1); // 1スキャンごとに待つ
     Wire.beginTransmission(address);
     uint8_t error = Wire.endTransmission();
 
     if (error != 0)
     {
-      return;
+      // エラーが発生した場合に詳細なメッセージを表示
+      // printI2CError(error, address);
+      continue; // 次のアドレスに進む
     }
 
     // データを要求
-    Wire.requestFrom(0x08, (uint8_t)2);
+    Wire.requestFrom(address, (uint8_t)2);
     if (Wire.available() >= 2)
     {
       int8_t sensorValue_MSB = Wire.read();
@@ -68,6 +106,4 @@ void loop()
       Serial.println(sensorValue);
     }
   }
-
-  delay(1); // 1スキャンごとに待つ
 }
